ee5308d2e2f8d21314eff77688fcd77edf8c9ad0
Jabir777 <jabir.pashaev@gmail.com>
2026-07-09 01:50:51 +0300

    подключить Prisma и PostgreSQL для авторизации (#5)

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,10 +1,58 @@
+import { registerFormSchema } from "@/src/features/register-form/model/registerFormSchema";
+import { prisma } from "@/src/shared/lib/prisma";
+import bcrypt from "bcryptjs";
+
 export async function POST(request: Request) {
   const body = await request.json();
 
-  console.log(body);
+  const validationResult = registerFormSchema.safeParse(body);
 
-  return Response.json({
-    message: "Данные получены",
-    body,
+  if (!validationResult.success) {
+    return Response.json(
+      {
+        message: "Некорректные данные",
+        errors: validationResult.error.flatten().fieldErrors,
+      },
+      { status: 400 },
+    );
+  }
+
+  const validatedData = validationResult.data;
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: validatedData.email,
+    },
   });
+
+  if (existingUser) {
+    return Response.json(
+      {
+        message: "Пользователь с таким email уже существует!",
+      },
+      { status: 409 },
+    );
+  }
+
+  const hashedPassword = await bcrypt.hash(validatedData.password, 10);
+
+  const createdUser = await prisma.user.create({
+    data: {
+      firstName: validatedData.firstName,
+      email: validatedData.email,
+      password: hashedPassword,
+    },
+  });
+
+  return Response.json(
+    {
+      message: "Пользователь успешно создан",
+      user: {
+        id: createdUser.id,
+        firstName: createdUser.firstName,
+        email: createdUser.email,
+      },
+    },
+    { status: 201 },
+  );
 }
