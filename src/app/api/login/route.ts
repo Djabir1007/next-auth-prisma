d2f75faf44c7e1566cd036602a48224d5945beaf
Djabir1007 <jabir.pashaev@gmail.com>
2026-07-18 23:20:17 +0300

    добавить JWT cookies при логине

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,6 +2,9 @@ import { loginFormSchema } from "@/src/features/login-form/model/loginFormSchema
 import { prisma } from "@/src/shared/lib/prisma";
 import bcrypt from "bcryptjs";
 
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
   const body = await request.json();
 
@@ -48,7 +51,23 @@ export async function POST(request: Request) {
     );
   }
 
-  return Response.json({
+  const accessToken = jwt.sign(
+    { userId: user.id },
+    process.env.JWT_ACCESS_SECRET!,
+    {
+      expiresIn: "15m",
+    },
+  );
+
+  const refreshToken = jwt.sign(
+    { userId: user.id },
+    process.env.JWT_REFRESH_SECRET!,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  const response = NextResponse.json({
     message: "Вход выполнен успешно",
     user: {
       id: user.id,
@@ -56,4 +75,22 @@ export async function POST(request: Request) {
       email: user.email,
     },
   });
+
+  response.cookies.set("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 60 * 15,
+    path: "/",
+  });
+
+  response.cookies.set("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 60 * 60 * 24 * 7,
+    path: "/",
+  });
+
+  return response;
 }
