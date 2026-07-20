0af79da08ced367a6cf12418926ee616d8c0813c
Djabir1007 <jabir.pashaev@gmail.com>
2026-07-20 17:10:04 +0300

    Добавить выход из аккаунта

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,12 @@
+import { cookies } from "next/headers";
+
+export async function POST() {
+  const cookieStore = await cookies();
+
+  cookieStore.delete("accessToken");
+  cookieStore.delete("refreshToken");
+
+  return Response.json({
+    message: "Выход выполнен успешно",
+  });
+}
