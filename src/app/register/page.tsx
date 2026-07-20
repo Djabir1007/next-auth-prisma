749182925e8bdc6bc38d46e85c2690fbbcd4ef80
Djabir1007 <jabir.pashaev@gmail.com>
2026-07-21 02:07:40 +0300

    Защитить страницы входа и регистрации

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,29 @@
 import { RegisterForm } from "@/src/features/register-form/ui/RegisterForm";
 import { Container } from "@/src/shared/ui/container/Container";
 
-const RegisterPage = () => {
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { redirect } from "next/navigation";
+
+const RegisterPage = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  let isAccessTokenValid = false;
+
+  if (accessToken) {
+    try {
+      jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET!);
+      isAccessTokenValid = true;
+    } catch {
+      isAccessTokenValid = false;
+    }
+  }
+
+  if (isAccessTokenValid) {
+    redirect("/profile");
+  }
+
   return (
     <section className="py-10">
       <Container>
