e9d8f4854cc8851456919190ed47f0321841edf6
Jabir777 <jabir.pashaev@gmail.com>
2026-07-21 02:09:21 +0300

    Защитить страницы входа и регистрации (#10)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,31 @@
 import { LoginForm } from "@/src/features/login-form/ui/LoginForm";
 import { Container } from "@/src/shared/ui/container/Container";
 
-const LoginPage = () => {
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { redirect } from "next/navigation";
+
+const LoginPage = async () => {
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  let isAccessTokenValid = false;
+
+  if (accessToken) {
+    try {
+      jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET!);
+
+      isAccessTokenValid = true;
+    } catch {
+      isAccessTokenValid = false;
+    }
+  }
+
+  if (isAccessTokenValid) {
+    redirect("/profile");
+  }
+
   return (
     <section>
       <Container>
